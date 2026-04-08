5c1710187328a0549e89d0469aa34566ddc81939
eriqueo <eriqueo@proton.me>
2026-04-08 10:49:44 -0600

    shell, ls fix

--- profiles/core.nix
+++ profiles/core.nix
@@ -49,6 +49,7 @@ in
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       ];
       auto-optimise-store = true;
+      trusted-users = [ "root" "eric" ];
     };
     gc = {
       automatic = true;
