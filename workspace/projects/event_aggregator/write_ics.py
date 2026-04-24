b03c12f60259a4368f471f9bf2a1cb0332214b59
Eric <eric@hwc.moe>
2026-04-24 14:01:11 -0600

    estimator

--- workspace/projects/event_aggregator/write_ics.py
+++ workspace/projects/event_aggregator/write_ics.py
@@ -1,7 +1,7 @@
 import sys, os
 filename = sys.argv[1]
 content = sys.stdin.read()
-path = f'/home/eric/000_inbox/downloads/events/{filename}'
+path = f'/home/eric/000_inbox/downloads/{filename}'
 with open(path, 'w') as f:
     f.write(content)
 print(f'Wrote: {path}')
